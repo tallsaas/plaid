8a76081e959dbc1155871b9e2774a12a4feb5e25
Joshua Anderton <info@joshanderton.com>
2022-02-27 15:29:27 -0800

    Initial commit

---
+++ src/Plaid.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TallSaas\Plaid;
+
+use Illuminate\Support\Facades\Http;
+
+class Plaid 
+{
+  //
+}
---
+++ src/Providers/PlaidServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TallSaas\Plaid\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PlaidServiceProvider extends ServiceProvider 
+{
+  public function boot()
+  {
+    //
+  }
+}
